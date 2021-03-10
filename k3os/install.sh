ed34aacd8a3194654c7c34aa38f8abb4948226e1
Kiefer Chang <kiefer.chang@suse.com>
2021-03-10 16:07:49 +0800

    K3OS: remove shutdown from install script
    To do things in the go code after running install script, we need to
    prevent the install script rebooting or shutting down.
    
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- k3os/install.sh
+++ k3os/install.sh
@@ -396,11 +396,3 @@ if [ -n "$INTERACTIVE" ]; then
     exit 0
 fi
 
-if [ "$K3OS_INSTALL_POWER_OFF" = true ] || grep -q 'k3os.install.power_off=true' /proc/cmdline; then
-    poweroff -f
-else
-    echo " * Installation completed"
-    echo " * Rebooting system in 5 seconds"
-    sleep 5
-    reboot -f
-fi
