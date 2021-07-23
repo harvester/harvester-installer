24b28fd4910e1728521d237acae343fdb73b1be4
Kiefer Chang <kiefer.chang@suse.com>
2021-07-23 23:48:22 +0800

    package images to ISO and preload from ISO
    - Move images out of squashfs image to reduce the squashfs image size.
      - The images are packaged in the `/bundle` directory in ISO.
    - Speedup the preloading by bind-mounting.
    - Switch back to docker save for exporting images.
    - Drop os2-tools images. Install luet-makeiso in the building container to
      generate ISOs.
    - Install new version of luet-makeiso that supports packaging files to the ISO.
    
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- package/harvester-os/files/usr/sbin/ctr-check-images.sh
+++ package/harvester-os/files/usr/sbin/ctr-check-images.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -eu
 # The script reads a image list and waits until images in the list are presented.
 
-sorted_list_file="/tmp.$(basename $1).sorted"
+sorted_list_file=$(mktemp)
 sort $1 > $sorted_list_file
 
 trap "rm -f $sorted_list_file" EXIT
 
+lines=$(wc -l < $sorted_list_file)
+echo Checking $lines images in $1...
+
 while true; do
     missing=$(ctr -n k8s.io images ls -q | grep -v ^sha256 | sort | comm -23 $sorted_list_file -)
     if [ -z "$missing" ]; then
+        echo done
         break
     fi
     sleep 2
