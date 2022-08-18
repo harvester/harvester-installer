f2f42525374b4f400218c0a5e6fc32a961e6e61c
Date Huang <date.huang@suse.com>
2022-08-18 14:21:34 +0800

    vlan: use bond-mgmt mac address directly and set it persisent
    Signed-off-by: Date Huang <date.huang@suse.com>

--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -5,7 +5,13 @@ INTERFACE=$2
 
 case $ACTION in
     pre-up)
+        # inherit MAC address from bond-mgmt
+        ip link set dev $INTERFACE address $(ip -json link show dev bond-mgmt | jq -j '.[0]["address"]')
+
+        # enable vlan-aware
         ip link set $INTERFACE type bridge vlan_filtering 1
+
+        # accept all vlan, PVID=1 by default
         bridge vlan add vid 2-4094 dev $INTERFACE self
         bridge vlan add vid 2-4094 dev bond-mgmt
         ;;
