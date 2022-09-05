1221880b9ef98748e65d0dcaa0a90bf12fba3ac9
Date Huang <date.huang@suse.com>
2022-09-05 10:16:53 +0800

    vlan: fit wicked pre/post up script behavior
    Signed-off-by: Date Huang <date.huang@suse.com>

--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -5,14 +5,16 @@ INTERFACE=$2
 
 case $ACTION in
     pre-up)
-        # inherit MAC address from {{ . }}
-        ip link set dev $INTERFACE address $(ip -json link show dev {{ . }} | jq -j '.[0]["address"]')
-
         # enable vlan-aware
         ip link set $INTERFACE type bridge vlan_filtering 1
+        ;;
+
+	post-up)
+        # inherit MAC address from {{ . }}
+        ip link set dev $INTERFACE address $(ip -json link show dev {{ . }} | jq -j '.[0]["address"]')
 
         # accept all vlan, PVID=1 by default
         bridge vlan add vid 2-4094 dev $INTERFACE self
         bridge vlan add vid 2-4094 dev {{ . }}
-        ;;
+		;;
 esac
