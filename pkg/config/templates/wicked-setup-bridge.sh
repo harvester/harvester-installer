2726b0c39e829ea6d7de532b3edb10224d0ca921
Date Huang <date.huang@suse.com>
2022-09-07 13:55:04 +0800

    vlan: fix indent to tab
    Signed-off-by: Date Huang <date.huang@suse.com>

--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -4,17 +4,17 @@ ACTION=$1
 INTERFACE=$2
 
 case $ACTION in
-    pre-up)
-        # enable vlan-aware
-        ip link set $INTERFACE type bridge vlan_filtering 1
-        ;;
+	pre-up)
+		# enable vlan-aware
+		ip link set $INTERFACE type bridge vlan_filtering 1
+		;;
 
 	post-up)
-        # inherit MAC address from {{ . }}
-        ip link set dev $INTERFACE address $(ip -json link show dev {{ . }} | jq -j '.[0]["address"]')
+		# inherit MAC address from {{ . }}
+		ip link set dev $INTERFACE address $(ip -json link show dev {{ . }} | jq -j '.[0]["address"]')
 
-        # accept all vlan, PVID=1 by default
-        bridge vlan add vid 2-4094 dev $INTERFACE self
-        bridge vlan add vid 2-4094 dev {{ . }}
+		# accept all vlan, PVID=1 by default
+		bridge vlan add vid 2-4094 dev $INTERFACE self
+		bridge vlan add vid 2-4094 dev {{ . }}
 		;;
 esac
