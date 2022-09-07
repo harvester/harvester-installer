21e0034a06dd0ba85b8c04110b7076380f8933d6
Date Huang <date.huang@suse.com>
2022-09-07 13:55:04 +0800

    vlan: fix indent to tab
    Signed-off-by: Date Huang <date.huang@suse.com>

--- pkg/config/templates/wicked-setup-bond.sh
+++ pkg/config/templates/wicked-setup-bond.sh
@@ -5,7 +5,7 @@ INTERFACE=$2
 
 case $ACTION in
 	post-up)
-        # accept all vlan, PVID=1 by default
-        bridge vlan add vid 2-4094 dev $INTERFACE
+		# accept all vlan, PVID=1 by default
+		bridge vlan add vid 2-4094 dev $INTERFACE
 		;;
 esac
