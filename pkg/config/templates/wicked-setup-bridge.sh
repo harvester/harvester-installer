8d8c3cc98d27d00201ff8a81efe30df3e67e542c
Date Huang <date.huang@suse.com>
2022-09-20 14:57:49 +0800

    vlan: apply review suggestion
    Signed-off-by: Date Huang <date.huang@suse.com>

--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -10,9 +10,6 @@ case $ACTION in
 		;;
 
 	post-up)
-		# inherit MAC address from {{ . }}
-		ip link set dev $INTERFACE address $(ip -json link show dev {{ . }} | jq -j '.[0]["address"]')
-
 		# accept all vlan, PVID=1 by default
 		bridge vlan add vid 2-4094 dev $INTERFACE self
 		bridge vlan add vid 2-4094 dev {{ . }}
