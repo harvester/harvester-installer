708630c754bcd4d3cc3ada931311e5680b14b63e
Chris Ho <chris.he@suse.com>
2023-05-23 15:06:06 +0800

    remove --kill-who flags

--- package/harvester-os/files/usr/sbin/rke2-kill-containers.sh
+++ package/harvester-os/files/usr/sbin/rke2-kill-containers.sh
@@ -13,7 +13,7 @@ if [ ${#unit_list[@]} -lt $max_concurrency ]; then
     max_concurrency=${#unit_list[@]}
 fi
 
-printf '%s\0' "${unit_list[@]}" | xargs -0 -n1 -P "$max_concurrency" sh -c 'sudo systemctl stop --kill-who=all "$@" && echo "Finished $@"' _ | { grep -v '^Finished' || true; } | { grep -v 'xargs:' || true; } &
+printf '%s\0' "${unit_list[@]}" | xargs -0 -n1 -P "$max_concurrency" sh -c 'sudo systemctl stop "$@" && echo "Finished $@"' _ | { grep -v '^Finished' || true; } | { grep -v 'xargs:' || true; } &
 wait
 
 echo "Finished stopping all units."
