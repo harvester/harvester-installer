f241d9146086f1c53150b7163d545cf5bb6f4493
Chris Chiu <chris.chiu@suse.com>
2024-03-11 16:17:11 +0800

    vlan: add PVID setting on the bridge
    Add PVID settings on the bridge in the pre-up script so the bridge
    can generate the specified VLAN traffic to get the corresponding
    DHCP IP address.
    
    It requires the VLAN setting restored every time when user apply
    new VLAN id for the management interface.
    
    Signed-off-by: Chris Chiu <chris.chiu@suse.com>

--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -7,11 +7,15 @@ case $ACTION in
 	pre-up)
 		# enable vlan-aware
 		ip link set $INTERFACE type bridge vlan_filtering 1
+		# reset to default vlan state
+		# accept all vlan, PVID=1 by default
+		bridge vlan del vid 1-4094 dev $INTERFACE self
+		# set the configured vlan id
+		{{ if and (gt . 1) (le . 4095) -}}
+		bridge vlan add vid {{ . }} dev $INTERFACE pvid untagged self
+		{{ else -}}
+		bridge vlan add vid 1 dev $INTERFACE pvid untagged self
+		{{ end -}}
 		;;
 
-	post-up)
-		# accept all vlan, PVID=1 by default
-		bridge vlan add vid 2-4094 dev $INTERFACE self
-		bridge vlan add vid 2-4094 dev {{ . }}
-		;;
 esac
