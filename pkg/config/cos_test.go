36ea2bf0c7f4e420b5ca4f1d1a75312240d1f9bc
Vicente Cheng <vicente.cheng@suse.com>
2024-07-19 15:43:30 +0800

    vendor: move `mudler/yip` to `rancher/yip`
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- pkg/config/cos_test.go
+++ pkg/config/cos_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	yipSchema "github.com/mudler/yip/pkg/schema"
+	yipSchema "github.com/rancher/yip/pkg/schema"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/harvester/harvester-installer/pkg/util"
