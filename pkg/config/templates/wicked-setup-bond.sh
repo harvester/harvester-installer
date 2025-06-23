2b3bc54503d7009400678ea07fcea1d69e3d5f73
Renuka Devi Rajendran <renuka.rajendran@suse.com>
2025-06-23 15:45:15 -0700

    Skip bridge vlan add when vid < 2
    Signed-off-by: Renuka Devi Rajendran <renuka.rajendran@suse.com>

--- pkg/config/templates/wicked-setup-bond.sh
+++ pkg/config/templates/wicked-setup-bond.sh
@@ -8,8 +8,8 @@ case $ACTION in
                 # inherit MAC address
                 ip link set dev {{ .IntfName }} address $(ip -json link show dev $INTERFACE | jq -j '.[0]["address"]')
 
-                #skip bridge vlan setting when no custom vlan specified by user
-                if [ {{ .VlanID }} -eq 0 ]; then
+                #skip bridge vlan setting when no vlan id or vlan id=1 specified by user
+                if [ {{ .VlanID }} -eq 0 ] || [ {{ .VlanID }} -eq 1 ]; then
                     exit 0
                 fi
                 #assign user configured vlan,PVID=1 by default
--- pkg/config/templates/wicked-setup-bridge.sh
+++ pkg/config/templates/wicked-setup-bridge.sh
@@ -10,8 +10,8 @@ case $ACTION in
                 ;;
 
         post-up)
-                #skip bridge vlan setting when no custom vlan specified by user
-                if [ {{ .VlanID }} -eq 0 ]; then
+                #skip bridge vlan setting when no vlan id or vlan-id=1 specified by user
+                if [ {{ .VlanID }} -eq 0 ] || [ {{ .VlanID }} -eq 1 ]; then
                     exit 0
                 fi
                 #assign user configured vlan,PVID=1 by default
